3ba9f609fd9c108223d15b3db70aa177058aa514
Samuel Mak <samuelswmak@gmail.com>
2017-08-11 04:43:34 -0700

    created a button menu to test reactive view change

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,6 +1,24 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Session } from 'meteor/session';
 
 import './body.html';
 import './dynamic-test.js';
 import './hello-info.js';
+
+Template.main.onCreated(function createViewSession() {
+  Session.set("view", "startmenu");
+  console.log("view set : ", Session.get("view"));
+});
+
+Template.main.helpers({
+  currentview: function() {
+    return Session.get("view");
+  }
+});
+
+// Template.buttonmenu.events({
+//   "click .newgame-button": function() {
+//     Session.set("view", "newgame");
+//   },
+// })
