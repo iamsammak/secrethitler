c2f50dd1e1b1c081cef3747d26dc74c67620c07e
Samuel Mak <samuelswmak@gmail.com>
2017-08-11 15:12:26 -0700

    new game view -room subscription complete

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -46,3 +46,17 @@ Template.buttonmenu.events({
     console.log(Session.get("view"));
   },
 });
+
+Template.newgame.events({
+  "click .back-btn": function() {
+    Session.set("view", "startmenu");
+  },
+  "submit #newgame-form": function(event) {
+    event.preventDefault();
+
+    console.log(event);
+    let name = event.target.name.value;
+    console.log("Hello", name);
+    console.log("add subscription to room and change view to lobby");
+  }
+});
