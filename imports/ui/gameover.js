a98b3d3311ce24c2b02fa5b08d92e516189114e8
Samuel Mak <samuelswmak@gmail.com>
2017-08-15 02:48:10 -0700

    add gameover template

--- imports/ui/gameover.js
+++ imports/ui/gameover.js
@@ -1,3 +1,17 @@
 import { Template } from 'meteor/templating';
+import { Rooms, Players } from '../api/collections.js';
 
 import './gameover.html';
+
+Template.gameover.helpers({
+  room: function() {
+    let roomId = Session.get("roomId");
+    let room = Rooms.findOne(roomId);
+    return room;
+  },
+  players: function() {
+    let roomId = Session.get("roomId");
+    let room = Rooms.findOne(roomId);
+    return room.players;
+  },
+})
