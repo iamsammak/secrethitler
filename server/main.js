f4279a726b4a7231b028eaee1dadd34e3b6811f9
Samuel Mak <samuelswmak@gmail.com>
2017-08-21 17:55:39 -0700

    missing } inside discard method, which fixes the policy choice bug

--- server/main.js
+++ server/main.js
@@ -303,7 +303,6 @@ Meteor.methods({
       discardpile: room.discardpile.concat(discarded),
       executiveaction: "inactive"
     };
-    console.log("discard", update);
 
     if (room.policychoices.length == 1) {
       console.log(room.policychoices);
@@ -312,110 +311,112 @@ Meteor.methods({
       } else if (room.policychoices[0] == "fascist") {
         update.fascist = room.fascist + 1;
 
-    // add executive action here
-      let party = room.players.length;
-      if (update.fascist == 1 || update.fascist == 2) {
-        if (party >= 9 || (party >= 7 && update.fascist == 2)) {
-        // if (party >= 3) { // replace this line with the above line when done testing
-          console.log("investigate loyalty");
-          update.executiveaction = "active";
-          update.investigate = true;
+        // add executive action here
+        let party = room.players.length;
+        if (update.fascist == 1 || update.fascist == 2) {
+          if (party >= 9 || (party >= 7 && update.fascist == 2)) {
+          // if (party >= 3) { // replace this line with the above line when done testing
+            console.log("investigate loyalty");
+            update.executiveaction = "active";
+            update.investigate = true;
 
-          let currPresId = room.players[room.currentPresident].playerId;
+            let currPresId = room.players[room.currentPresident].playerId;
 
-          // create an array of players except president
-          let suspects = _.filter(room.players, function(player) {
-            return player.playerId != currPresId;
-          });
+            // create an array of players except president
+            let suspects = _.filter(room.players, function(player) {
+              return player.playerId != currPresId;
+            });
 
-          update.suspects = suspects;
+            update.suspects = suspects;
+          }
         }
-      }
-      if (update.fascist == 3) {
-        if (party >= 7) { // change back to 7
-          console.log("call special election");
+        if (update.fascist == 3) {
+          if (party >= 7) { // change back to 7
+            console.log("call special election");
 
-          update.specialelection = true;
-          update.executiveaction = "active";
+            update.specialelection = true;
+            update.executiveaction = "active";
 
 
-        } else if (party >= 5) { // change back to 5
-          console.log("policy peek");
-          let peek = room.drawpile.slice(0, 3);
-          update.peek = peek;
-          update.executiveaction = "active";
+          } else if (party >= 5) { // change back to 5
+            console.log("policy peek");
+            let peek = room.drawpile.slice(0, 3);
+            update.peek = peek;
+            update.executiveaction = "active";
+          }
         }
-      }
-      if (update.fascist == 4) { // change back to == 4
-        if (party >= 5) { // change back to 5
+        if (update.fascist == 4) { // change back to == 4
+          if (party >= 5) { // change back to 5
 
-          update.executiveaction = "active";
-          update.assassination = true;
-          console.log("execution", update);
+            update.executiveaction = "active";
+            update.assassination = true;
+            console.log("execution", update);
+          }
         }
-      }
-      if (update.fascist == 5) { // change back to update.fascist == 5
-        if (party >= 5) { // change back to 5
-          console.log("execution and veto unlocked");
+        if (update.fascist == 5) { // change back to update.fascist == 5
+          if (party >= 5) { // change back to 5
+            console.log("execution and veto unlocked");
 
-          // veto - not an executive special power so executiveaction still inactive
-          update.vetobutton = { president: true, chancellor: true };
-          update.vetoresult = { president: "", chancellor: "" };
+            // veto - not an executive special power so executiveaction still inactive
+            update.vetobutton = { president: true, chancellor: true };
+            update.vetoresult = { president: "", chancellor: "" };
+          }
         }
-      }
-      // reset the room, move round forward and move president placard
-      if (update.executiveaction == "inactive") {
-        update.round = room.round + 1;
-        update.voted = false;
-        update.votes = {};
-        update.voteresult = "";
-        update.currentPresident = (room.currentPresident + 1) % _.size(room.players);
-        update.currentChancellor = -1
-        if (room.alive <= 3) {
-          update.ruledout = [
-            room.players[room.currentChancellor].playerId ];
-        } else {
-          update.ruledout = [
-            room.players[room.currentPresident].playerId,
-            room.players[room.currentChancellor].playerId ];
-        }
-        // TODO insert skip dead people as president
-        if (room.deadindex.length != 0) {
+        // reset the room, move round forward and move president placard
+        if (update.executiveaction == "inactive") {
+          update.round = room.round + 1;
+          update.voted = false;
+          update.votes = {};
+          update.voteresult = "";
           update.currentPresident = (room.currentPresident + 1) % _.size(room.players);
-          while (_.contains(room.deadindex, update.currentPresident)) {
-            console.log(update.currentPresident);
-            update.currentPresident = (update.currentPresident + 1) % _.size(room.players);
+          update.currentChancellor = -1
+
+          if (room.alive <= 3) {
+            update.ruledout = [
+              room.players[room.currentChancellor].playerId ];
+          } else {
+            update.ruledout = [
+              room.players[room.currentPresident].playerId,
+              room.players[room.currentChancellor].playerId ];
+          }
+          // skip the dead while passing President placard
+          if (room.deadindex.length != 0) {
+            update.currentPresident = (room.currentPresident + 1) % _.size(room.players);
+            while (_.contains(room.deadindex, update.currentPresident)) {
+              console.log(update.currentPresident);
+              update.currentPresident = (update.currentPresident + 1) % _.size(room.players);
+            }
+          }
+        }
+        // reset president rotation to prior special election
+        if (room.resetspecialelection.length != 0) {
+          console.log("inside reset special election, discard");
+          update.currentPresident = (room.resetspecialelection[0] + 1) % _.size(room.players);
+          update.resetspecialelection = [];
+        }
+
+        // end of game
+        if (update.liberal == 5 || update.fascist == 6) {
+          update.state = "gameover";
+          if (update.liberal == 5) {
+            update.winner = "liberals";
+            update.reason = "liberals have passed 5 policies!";
+          } else if (update.fascist == 6) {
+            update.winner = "fascists";
+            update.reason = "fascists have passed 6 policies!";
+          }
+          update.players = room.players;
+          for (let i = 0; i < room.players.length; i += 1) {
+            update.players[i].side = Players.findOne(room.players[i].playerId).role;
           }
         }
       }
-
-      if (room.resetspecialelection.length != 0) {
-        console.log("inside reset special election, discard");
-        update.currentPresident = (room.resetspecialelection[0] + 1) % _.size(room.players);
-        update.resetspecialelection = [];
-      }
-
-    // end of game
-      if (update.liberal == 5 || update.fascist == 6) {
-        update.state = "gameover";
-        if (update.liberal == 5) {
-          update.winner = "liberals";
-          update.reason = "liberals have passed 5 policies!";
-        } else if (update.fascist == 6) {
-          update.winner = "fascists";
-          update.reason = "fascists have passed 6 policies!";
-        }
-        update.players = room.players;
-        for (let i = 0; i < room.players.length; i += 1) {
-          update.players[i].side = Players.findOne(room.players[i].playerId).role;
-        }
-      }
+      // policy is enacted
+      // reset tracker and policychoices despite executive action or not
+      update.policychoices = [];
+      update.electiontracker = 0;
     }
-
-    // policy is enacted therefore, reset the following despite executive action or not
-    update.policychoices = [];
-    update.electiontracker = 0;
-
+    console.log("discard", update);
     Rooms.update(player.roomId, { $set: update });
   },
   "investigate" ({ suspectId }) {
