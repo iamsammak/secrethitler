2b2f5d52fbc278af7c0e0dacab891ddb639bed74
Samuel Mak <samuelswmak@gmail.com>
2017-09-15 15:23:28 -0700

    add keep awake dyno

--- server/main.js
+++ server/main.js
@@ -8,6 +8,10 @@ Meteor.startup(() => {
   // code to run on server at startup
   // add clean up game and players logic
   // install mrt: cron-tick and use cron to schedule game and player removal after set time duration
+  let http = require("http");
+  setInterval(function() {
+      http.get("http://secrethitler-ubiquitousfriends.herokuapp.com");
+  }, 300000); // every 5 minutes (300000)
 });
 
 Meteor.publish("rooms", function(code) {
